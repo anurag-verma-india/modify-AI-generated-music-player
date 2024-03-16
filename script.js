a3fb708ae1b550326197adfaa906527b3f28c2c2
Anurag Verma <anuragsoniemail@gmail.com>
2024-03-16 18:53:21 +0530

    Added Songs, fixed responsive issues

--- script.js
+++ script.js
@@ -10,33 +10,33 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    { songName: "Libaas", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Dope Shope", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
+    { songName: "Why this kolavari di", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Pasoori Nu", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { songName: "Channa mereya unplugged", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+    { songName: "Maan Meri Jaan - Modi ji x King", filePath: "songs/6.mp3", coverPath: "covers/7.jpg" },
+    { songName: "Saadi Gali Aaja", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" },
+    { songName: "Chaleya", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" },
+    { songName: "Blue Eyes", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" },
+    { songName: "Apna bana le piya", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" },
 ]
 
-songItems.forEach((element, i)=>{ 
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
 })
- 
+
 
 // Handle play/pause click
-masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
         gif.style.opacity = 1;
     }
-    else{
+    else {
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
@@ -44,30 +44,30 @@ masterPlay.addEventListener('click', ()=>{
     }
 })
 // Listen to Events
-audioElement.addEventListener('timeupdate', ()=>{ 
+audioElement.addEventListener('timeupdate', () => {
     // Update Seekbar
-    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
     myProgressBar.value = progress;
 })
 
-myProgressBar.addEventListener('change', ()=>{
-    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
 })
 
-const makeAllPlays = ()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
         element.classList.remove('fa-pause-circle');
         element.classList.add('fa-play-circle');
     })
 }
 
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click', (e)=>{ 
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
         makeAllPlays();
         songIndex = parseInt(e.target.id);
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
+        audioElement.src = `songs/${songIndex + 1}.mp3`;
         masterSongName.innerText = songs[songIndex].songName;
         audioElement.currentTime = 0;
         audioElement.play();
@@ -77,14 +77,14 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     })
 })
 
-document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>=9){
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= 9) {
         songIndex = 0
     }
-    else{
+    else {
         songIndex += 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
@@ -93,14 +93,14 @@ document.getElementById('next').addEventListener('click', ()=>{
 
 })
 
-document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
         songIndex = 0
     }
-    else{
+    else {
         songIndex -= 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
